61601deaa9c736c1a2d339b8576484ed04e23165
Eric Loudon <loudon.eric@gmail.com>
2019-08-26 07:56:37 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,12 +11,11 @@ class EmailParser
 
 	def parse
 		@emails = @emails.gsub(/[\s,]/ ," ").split
-		# @email is an array now
 		@emails.uniq
 	end
 
 end
 
- #emails = "john@doe.com, person@somewhere.org"
-  #parser = EmailParser.new(emails)
-  #parser.parse => ["@john@doe.com", "person@somewhere.org"]
+emails = "john@doe.com, person@somewhere.org"
+parser = EmailParser.new(emails)
+parser.parse => ["@john@doe.com", "person@somewhere.org"]
