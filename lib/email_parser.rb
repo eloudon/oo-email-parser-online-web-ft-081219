f47a1e5389fbf34e5f4253c3ef8d61713f507c10
Eric Loudon <loudon.eric@gmail.com>
2019-08-26 07:55:48 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,17 +4,17 @@
 # or whitespace (' ').
 class EmailParser
 	attr_accessor :emails
-	
+
 	def initialize(emails)
 		@emails= emails
 	end
-	
+
 	def parse
 		@emails = @emails.gsub(/[\s,]/ ," ").split
 		# @email is an array now
 		@emails.uniq
 	end
-	
+
 end
 
  #emails = "john@doe.com, person@somewhere.org"
